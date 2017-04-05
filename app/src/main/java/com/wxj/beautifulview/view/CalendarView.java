a890d578130a569bf00bc8ac390f22b1a96172aa
wxj978566042 <wxj978566042@gmail.com>
2017-04-05 16:48:49 +0800

    it's a calendar view.

---
+++ app/src/main/java/com/wxj/beautifulview/view/CalendarView.java
@@ -0,0 +1,24 @@
+package com.wxj.beautifulview.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * User: W03673
+ * Date: 2017/4/5.
+ * Time: 16:47.
+ * Descriptions:A Calendar View
+ */
+
+public class CalendarView extends View {
+
+    public CalendarView(Context context) {
+        super(context);
+    }
+
+    public CalendarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
